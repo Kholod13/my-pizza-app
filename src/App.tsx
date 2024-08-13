e3a312a395cb100970be0cdf1a731a2cbe45d601
Kholod13 <111706893+Kholod13@users.noreply.github.com>
2024-08-13 18:44:46 +0200

    start laravel

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import CategoriesPage from './components/categories/CategoriesPage'
+
+function App() {
+ 
+  return (
+    <>
+      <CategoriesPage/>\
+      
+    </>
+  )
+}
+
+
+export default App
