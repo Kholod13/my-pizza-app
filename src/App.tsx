51c800c71ad64806f1b482f12b4e787b9f859870
Kholod13 <111706893+Kholod13@users.noreply.github.com>
2024-08-22 20:26:16 +0200

    fix bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
 import './App.css'
-import CategoriesPage from './components/categories/CategoriesPage'
-
+import CategoriesPage from "./components/categories/CategoriesPage.tsx";
+import {Route, Routes} from "react-router-dom";
+import CategoryCreatePage from './components/create';
 function App() {
- 
+
+
   return (
     <>
-      <CategoriesPage/>\
-      
+        <Routes>
+            <Route path="/" >
+                <Route index element={<CategoriesPage />} />
+                <Route path={"create"} element={<CategoryCreatePage />} />
+            </Route>
+        </Routes>
     </>
   )
 }
 
-
 export default App
+
